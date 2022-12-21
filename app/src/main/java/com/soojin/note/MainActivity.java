83145f901f24949557e218bde9e419913163948d
heosujinnn <0204sujin@naver.com>
2022-12-22 06:33:56 +0900

    notesApp

---
+++ app/src/main/java/com/soojin/note/MainActivity.java
@@ -0,0 +1,184 @@
+package com.soojin.note;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.SearchView;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.room.Room;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.soojin.note.Adapters.NotesListAdapter;
+import com.soojin.note.Database.RoomDB;
+import com.soojin.note.Models.Notes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
+    RecyclerView recyclerView;
+    NotesListAdapter notesListAdapter;
+    List<Notes> notes=new ArrayList<>();
+    RoomDB dataBase;
+    FloatingActionButton fab_add;
+
+    SearchView searchView_home;
+
+    Notes selectedNote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+
+        searchView_home=findViewById(R.id.searchView_home);
+
+        dataBase = RoomDB.getInstance(this);
+        notes = dataBase.mainDAO().getAll();
+
+        updateRecycler(notes);
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+                resultLauncher.launch(intent);
+            }
+
+        });
+        searchView_home.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return true;
+            }
+        });
+
+    }
+
+    private void filter(String newText) {
+        List<Notes> filteredList=new ArrayList<>();
+        for(Notes singleNote : notes){
+            if(singleNote.getTitle().toLowerCase().contains(newText.toLowerCase())
+                    || singleNote.getNotes().toLowerCase().contains(newText.toLowerCase())){
+                filteredList.add(singleNote);
+            }
+        }
+        notesListAdapter.filterList(filteredList);
+    }
+
+
+    private final ActivityResultLauncher<Intent> resultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        Notes new_notes = (Notes) data.getSerializableExtra("note");
+                        dataBase.mainDAO().insert(new_notes);
+                        notes.clear();
+                        notes.addAll(dataBase.mainDAO().getAll());
+                        notesListAdapter.notifyDataSetChanged();
+
+                    }
+                });
+
+
+
+
+
+
+    private void updateRecycler(List<Notes> notes) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)); //2열로 세로로로
+        notesListAdapter=new NotesListAdapter(MainActivity.this, notes,notesClickListener);
+        recyclerView.setAdapter(notesListAdapter);
+
+
+    }
+
+    private final NotesClickListener notesClickListener=new NotesClickListener() {
+        @Override
+        public void onclick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+            selectedNote=new Notes();
+            selectedNote=notes;
+            showPopup(cardView);
+        }
+    };
+
+    private void showPopup(CardView cardView) {
+        PopupMenu popupMenu=new PopupMenu(this, cardView);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.pin:
+                if(selectedNote.isPinned()){
+                    dataBase.mainDAO().pin(selectedNote.getID(),false);
+                    overridePendingTransition(0,0);
+                    startActivity(getIntent());
+                    overridePendingTransition(0,0);
+                    Toast.makeText(MainActivity.this,"고정안됨!",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    dataBase.mainDAO().pin(selectedNote.getID(),true);
+                    overridePendingTransition(0,0);
+                    startActivity(getIntent());
+                    overridePendingTransition(0,0);
+                    Toast.makeText(MainActivity.this,"고정됨!",Toast.LENGTH_SHORT).show();
+                }
+                notes.clear();
+                notes.addAll(dataBase.mainDAO().getAll());
+                notesListAdapter.notifyDataSetChanged();
+                //새로고침
+                overridePendingTransition(0,0);
+                startActivity(getIntent());
+                overridePendingTransition(0,0);
+                return true;
+
+            case R.id.delete:
+                dataBase.mainDAO().delete(selectedNote);
+                notes.remove(selectedNote);
+                notesListAdapter.notifyDataSetChanged();
+                overridePendingTransition(0,0);
+                startActivity(getIntent());
+                overridePendingTransition(0,0);
+                Toast.makeText(MainActivity.this,"메모가 삭제되었습니다.",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+
+        }
+
+    }
+}
